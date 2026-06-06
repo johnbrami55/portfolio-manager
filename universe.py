5e57a42214e159ec47e28ba9739824e94e621280
johnbrami55 <johnbrami@googlemail.com>
2026-06-06 10:58:52 +0200

    Update universe.py

--- universe.py
+++ universe.py
@@ -10,7 +10,7 @@ from config import (
 )
 
 logger = logging.getLogger(__name__)
-RAPIDAPI_HOST = "yahoo-finance187.p.rapidapi.com"
+RAPIDAPI_HOST = "yh-finance.p.rapidapi.com"
 def get_headers():
     return {
         "x-rapidapi-key": os.environ.get("RAPIDAPI_KEY", ""),
