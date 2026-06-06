4da71ff7745622a4afdc8ab4ed040b68333b9825
johnbrami55 <johnbrami@googlemail.com>
2026-06-06 10:59:50 +0200

    Update regime.py

--- regime.py
+++ regime.py
@@ -6,7 +6,7 @@ from config import CAC40_TICKER, STOXX600_TICKER, MA_SHORT, MA_LONG
 
 logger = logging.getLogger(__name__)
 
-RAPIDAPI_HOST = "yahoo-finance187.p.rapidapi.com"
+RAPIDAPI_HOST = "yh-finance.p.rapidapi.com"
 
 def get_headers():
     return {
